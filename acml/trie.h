11fdebf990ca50355feb3fba0c47b93dca01d568
Aleksandr Petushkov <aleks.petushkov@gmail.com>
2015-12-25 17:39:03 +0600

    begine

---
+++ acml/trie.h
@@ -0,0 +1,22 @@
+#include "Node.h"
+#include  <string>
+
+template <class T, class T2>class Trie
+{
+	Trie()
+	{
+		Node<T2> root;
+	}
+	void Add(T _new)
+	{
+		for()
+		for (bool b = true; b;)
+		{
+			for(const auto elem:_new)
+			{
+				
+			}
+		}
+	}
+
+};
