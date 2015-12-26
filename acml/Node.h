74331fe73efa415a7a149a4506a30ef54bd233e6
Aleksandr Petushkov <aleks.petushkov@gmail.com>
2015-12-26 23:57:50 +0600

    0.0.1

--- acml/Node.h
+++ acml/Node.h
@@ -1,8 +1,61 @@
+#include <map>
+#include <vector>
 
 
-
-
-template<class T> class Node
+class Node
 {
+public:
 	Node(){}
+	Node(char _elem)
+	{
+		_val = _elem;
+	}
+
+	bool find(const char& in)
+	{
+		if(list.find(in)!=list.end())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	Node* next(char _val)
+	{
+		return	&list[_val];	
+	}
+	void add(char _elem)
+	{
+		Node nNode(_elem);
+		list[_elem] = nNode;
+	}
+	bool End()const
+	{
+		return _end;
+	}
+	void Set_end()
+	{
+		_end = true;
+	}
+	std::vector<std::string> get_exist_end()
+	{
+		std::vector<std::string> result;
+		std::string tmp;
+		 for(const auto& elem:list)
+		 {
+			 if (elem.second.End())
+			 {
+				 tmp = elem.first;
+				 result.push_back(tmp);
+			 }
+
+		 }
+	}
+ protected:
+	char _val;
+	bool _end;
+	std::map<char, Node> list;
 };
--- acml/trie.h
+++ acml/trie.h
@@ -1,22 +1,63 @@
 #include "Node.h"
 #include  <string>
+#include <vector>
+#include <set>
+#include <string>
 
-template <class T, class T2>class Trie
+class Trie
 {
 	Trie()
 	{
-		Node<T2> root;
+
+		
 	}
-	void Add(T _new)
+	void Add(std::string _new)
 	{
-		for()
-		for (bool b = true; b;)
+		current = &root;
+
+		for (unsigned short int i = 0; i < _new.size();++i)
 		{
-			for(const auto elem:_new)
+			if(current->find(_new[i]))
 			{
-				
+				current = current->next(_new[i]);
+			}
+			else
+			{
+				current->add(_new[i]);
+				current = current->next(_new[i]);
 			}
 		}
+		current->Set_end();
 	}
 
+
+	std::vector<std::string> search(const std::string& _str, const char& size_mass=5)
+	{
+		std::vector<std::string> result;
+		for (unsigned short int i = 0; i < _str.size(); ++i)
+		{
+			if (current->find(_str[i]))
+			{
+				current = current->next(_str[i]);
+			}
+			else
+			{
+				return result;
+			}
+		}
+
+		std::vector<std::string> mass_ends = current->get_exist_end();
+		
+		for (char i = size_mass; i > 0;--i)
+		{
+			std::string tmp = _str;
+			n_current = current;
+			for(n_current)
+		}     
+	}
+
+protected:
+	Node root;
+	Node* current;
+	
 };
